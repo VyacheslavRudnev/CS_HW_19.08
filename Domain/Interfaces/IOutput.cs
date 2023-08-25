a4efc03bc93b669ea939ae51edbf82557accd568
Rudnev Vyacheslav <v.rudnev31@gmail.com>
2023-08-25 16:50:52 +0300

    HW task 04 done

--- Domain/Interfaces/IOutput.cs
+++ Domain/Interfaces/IOutput.cs
@@ -1,6 +1,4 @@
-﻿//using Woker = ConsoleApp190823.Domain.Models.Woker;
-
-namespace ConsoleApp190823.Domain.Interfaces
+﻿namespace ConsoleApp190823.Domain.Interfaces
 {
     public interface IOutput<T>
     {
